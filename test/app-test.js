2d18865ed781a14c747b3aed73cd414470461f38
Matt Schofield <matt.schofield.102@gmail.com>
2014-05-22 16:26:03 +0100

    Initial commit

---
+++ test/app-test.js
@@ -0,0 +1,11 @@
+var mocha = require('mocha')
+  , should = require('should');
+
+describe('test', function () {
+	
+	it('should work', function () {
+		var one = 1;
+    one.should.equal(1);
+	});
+
+});
