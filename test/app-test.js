31a28532df421f4cb5464f43540220ae075d0716
Matt Schofield <matt.schofield.102@gmail.com>
2014-05-22 17:27:18 +0100

    Added some test

--- test/app-test.js
+++ test/app-test.js
@@ -6,11 +6,16 @@ describe('test', function () {
 	it('should work', function () {
 		var one = 1;
     one.should.equal(1);
-	});
+	})
 
   it('should be cool', function () {
     var yes = true;
     yes.should.be.ok;
+  })
+
+  it('should be funky', function () {
+    var array = [1,2,3];
+    array.length.should.be.equal(3);
   })
 
 });
